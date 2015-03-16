eeb822b3195d4f5bd3a5e5fac56bbb328e6c8c0e
Chris Simpkins <chris.simpkins@gmail.com>
2015-03-15 22:16:46 -0400

    Added .emacs.d symlink.

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -3,3 +3,4 @@ export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -sfv "$DOTFILES_DIR/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/.mbsyncrc" ~
+ln -sfv "$DOTFILES_DIR/.emacs.d" ~
