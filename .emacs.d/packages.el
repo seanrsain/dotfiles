1bb1b158c2ba52b85c0b6df0d63694b61c198ff2
Chris Simpkins <chris.simpkins@gmail.com>
2015-07-24 18:00:39 -0400

    Added magit, git-gutter. Removed yasnippet.

--- .emacs.d/packages.el
+++ .emacs.d/packages.el
@@ -1,24 +1,28 @@
 ;; MELPA package manager
 (require 'package)
 
-;; Override the various Emacsen, e.g. Aquamacs, so all versions and all users can share
+;; Override the various installed Emacsen so all versions and users can share
 (setq package-user-dir "/usr/local/share/emacs/elpa")
 
 (add-to-list 'package-archives
-         '("melpa" . "http://melpa.milkbox.net/packages/") t)
+         '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(setq package-list '(ensime yasnippet expand-region multiple-cursors graphviz-dot-mode))
+(setq package-list
+      '(ensime
+        expand-region
+        multiple-cursors
+        graphviz-dot-mode
+        magit
+        git-gutter-fringe+))
+
 (mapc 'package-install package-list)
 
 (require 'ensime)
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
-
-(require 'yasnippet)
-(yas-global-mode 1)
 
 (require 'expand-region)
 (global-set-key (kbd "C-=") 'er/expand-region)
@@ -29,3 +33,6 @@
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
 (require 'graphviz-dot-mode)
+
+(require 'git-gutter-fringe+)
+(global-git-gutter+-mode t)
