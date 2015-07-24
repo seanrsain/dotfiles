014bf33aee8277aeecbf20e971374cc4488ef970
Chris Simpkins <chris.simpkins@gmail.com>
2015-07-24 17:59:45 -0400

    Fixed tabbar-mode for Aquamacs. Added smooth scroll settings.

--- .emacs.d/options.el
+++ .emacs.d/options.el
@@ -1,27 +1,30 @@
 ;; Basics
-(setq visible-bell 1)
+(setq-default visible-bell 1)
 (setq-default indent-tabs-mode nil)
-(setq show-paren-mode 1)
-(if window-system
-    (progn (tool-bar-mode 0)
-           (tabbar-mode 0)))
+(setq-default show-paren-mode 1)
+(if window-system (tool-bar-mode 0))
+(if (boundp 'aquamacs-version) (tabbar-mode 0))
 (global-linum-mode t)
-(setq linum-format "%3d ")
-(setq visual-line-mode t)
-(setq word-wrap t)
+(setq-default linum-format "%4d ")
+(add-hook 'text-mode-hook 'turn-on-visual-line-mode)
+(setq-default visual-line-fringe-indicators '(nil nil))
+(setq-default word-wrap t)
+(setq-default column-number-mode t)
 (setq-default ispell-program-name "/usr/local/bin/ispell")
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 (global-set-key (kbd "M-j") 'join-line)
 (global-set-key (kbd "M-g") 'goto-line)
 
-;; Try to get rid of Aquamacs's insistence on turning on auto-fill everywhere
-(turn-off-auto-fill)
-(remove-hook 'text-mode-hook #'turn-on-auto-fill)
+;; From http://www.emacswiki.org/emacs/SmoothScrolling
+;; Scroll one line at a time (less "jumpy" than defaults)
+(setq-default mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+(setq-default mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq-default mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq-default scroll-step 1) ;; keyboard scroll one line at a time
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Org-mode
-;; (setq org-default-notes-file (concat org-directory "~/Documents/capture.org"))
-;;      (define-key global-map "\C-cc" 'org-capture)
+;; Try to get rid of Aquamacs's insistence on turning on auto-fill everywhere
+;; (turn-off-auto-fill)
+;; (remove-hook 'text-mode-hook #'turn-on-auto-fill)
 
 ;; Graphviz dot language
 (org-babel-do-load-languages
