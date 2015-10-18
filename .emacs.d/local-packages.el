ade8d9f85223ca0537159b3213124adbbaa26e95
Chris Simpkins <chris.simpkins@gmail.com>
2015-10-18 10:39:22 -0400

    Rename local-extesions.el to local-packages.el, add Homebrew site-lisp

--- .emacs.d/local-packages.el
+++ .emacs.d/local-packages.el
@@ -1,5 +1,9 @@
 ;; Extensions that don't have ELPA packages
 
+;; Homebrew-installed packages
+(let ((default-directory "/usr/local/share/emacs/site-lisp/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
 ;; markdown-mode
 (add-to-list 'load-path "~/vcs/github.com/jrblevin/markdown-mode")
 
