5f0701929f85bb37fbd7d3f99197711546b688b5
Chris Simpkins <chris.simpkins@gmail.com>
2015-10-28 18:42:51 -0400

    Remove awful markdown-mode

--- .emacs.d/local-packages.el
+++ .emacs.d/local-packages.el
@@ -3,12 +3,3 @@
 ;; Homebrew-installed packages
 (let ((default-directory "/usr/local/share/emacs/site-lisp/"))
   (normal-top-level-add-subdirs-to-load-path))
-
-;; markdown-mode
-(add-to-list 'load-path "~/vcs/github.com/jrblevin/markdown-mode")
-
-(autoload 'markdown-mode "markdown-mode"
-   "Major mode for editing Markdown files" t)
-
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
