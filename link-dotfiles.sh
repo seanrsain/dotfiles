e80a1d6919a58a536472fdb4465d086ce8f29879
Chris Simpkins <chris.simpkins@gmail.com>
2015-12-30 21:46:14 -0500

    Move system-created .emacs.d out of the way

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -1,6 +1,5 @@
-
+mv ~/.emacs.d ~/.emacs.d.$(date +%F)
 export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ln -sfv "$DOTFILES_DIR/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/.mbsyncrc" ~
 ln -sfv "$DOTFILES_DIR/.emacs.d" ~
