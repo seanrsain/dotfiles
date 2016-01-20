924336aa06e0f034fb9942a5545a38b1b248ca29
Chris Simpkins <chris.simpkins@gmail.com>
2016-01-20 00:20:59 -0500

    Add .profile

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -1,5 +1,6 @@
 mv ~/.emacs.d ~/.emacs.d.$(date +%F)
 export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ln -sfv "$DOTFILES_DIR/.profile" ~
 ln -sfv "$DOTFILES_DIR/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/.gitignore" ~
 ln -sfv "$DOTFILES_DIR/.mbsyncrc" ~
