82a0e7d7467ab71011cd2043b181896200b6201c
Chris Simpkins <chris.simpkins@gmail.com>
2016-08-28 22:41:26 -0400

    Update scala, sbt installs

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -23,15 +23,17 @@ apt-get -y install texlive-full
 # Programming languages
 apt-get -y install openjdk-8-jdk
 apt-get -y install openjfx
-apt-get -y install scala
-echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
-apt-get -y update
-apt-get -y install sbt
-apt-get -y install typesafe-activator
-apt-get -y install scalastyle
 apt-get -y install ocaml
 apt-get -y install opam
+
+
+# For scala, get the latest .deb from scala-lang.org and install with dpkg
+
+# sbt
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+sudo apt-get update
+sudo apt-get install sbt
 
 # Applications
 apt-get install gimp
